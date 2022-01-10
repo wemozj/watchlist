72dea699d598b514d6a5623870b6cdcb2e64f213
jun.zhu <jun.zhu@shopee.com>
2022-01-10 21:43:03 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -5,38 +5,40 @@ from flask import Flask, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 WIN = sys.platform.startswith('win')
-if WIN: # 如果是 Windows 系统，使用三个斜线
+if WIN:  # 如果是 Windows 系统，使用三个斜线
     prefix = 'sqlite:///'
-else: # 否则使用四个斜线
+else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
 db = SQLAlchemy(app)  # 初始化扩展类，传入程序实例
 
 
-class User(db.Model): # 表名将会是 user（自动生成，小写处理）
-    id = db.Column(db.Integer, primary_key=True) # 主键
-    name = db.Column(db.String(20)) # 名字
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
 
 
-class Movie(db.Model): # 表名将会是 movie
-    id = db.Column(db.Integer, primary_key=True) # 主键
-    title = db.Column(db.String(60)) # 电影标题
-    year = db.Column(db.String(4)) # 电影年份
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
 
 
 import click
-@app.cli.command() # 注册为命令
+
+
+@app.cli.command()  # 注册为命令
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
 def initdb(drop):
     """Initialize the database."""
-    if drop: # 判断是否输入了选项
+    if drop:  # 判断是否输入了选项
         db.drop_all()
     db.create_all()
-    click.echo('Initialized database.') # 输出提示信息
+    click.echo('Initialized database.')  # 输出提示信息
 
 
 @app.cli.command()
@@ -61,37 +63,41 @@ def forge():
     db.session.add(user)
     for m in movies:
         movie = Movie(title=m['title'], year=m['year'])
-    db.session.add(movie)
+        db.session.add(movie)
     db.session.commit()
     click.echo('Done.')
 
 
-name = 'Zhu Zhu'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-
+# name = 'Zhu Zhu'
+# movies = [
+#     {'title': 'My Neighbor Totoro', 'year': '1988'},
+#     {'title': 'Dead Poets Society', 'year': '1989'},
+#     {'title': 'A Perfect World', 'year': '1993'},
+#     {'title': 'Leon', 'year': '1994'},
+#     {'title': 'Mahjong', 'year': '1996'},
+#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#     {'title': 'King of Comedy', 'year': '1999'},
+#     {'title': 'Devils on the Doorstep', 'year': '1999'},
+#     {'title': 'WALL-E', 'year': '2008'},
+#     {'title': 'The Pork of Music', 'year': '2012'},
+# ]
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要{'user': user}
 
 
 @app.route('/')
 def index():
-    user = User.query.first() #  读取用户记录
-    movies = Movie.query.all() #  读取所有电影记录
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
     # return 'Welcome to My Watchlist!'
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
-@app.route('/html')
-def show():
-    return '<h1>Hello zhuzhu!</h1><img src="http://helloflask.com/totoro.gif">'
+
+# @app.route('/html')
+# def show():
+#     return '<h1>Hello zhuzhu!</h1><img src="http://helloflask.com/totoro.gif">'
 
 
 @app.route('/user/<name>')
@@ -108,4 +114,19 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  # 输出: /test?num=2
 
     return 'Test page'
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):   # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+
+
+
+
+
+
+
+
 
